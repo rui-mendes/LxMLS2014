3e4ee4dfcf5e5c54f3b96b4cab69d5d2fa0ec9aa
Rui Mendes <ruidamendes@ua.pt>
2014-07-22 16:27:31 +0100

    Added info to exercise0.11

--- Day0/exerc-0.11.py
+++ Day0/exerc-0.11.py
@@ -11,6 +11,9 @@ plt.ylim(-5, 15)
 k = np.array([-2, 0, 2])
 plt.plot(k, k**2, "bo")
 for i in k:
+    # The equation of the tangent line to y = f(x) at the point (x1,y1):
+    # ( 'm' is the gradient at (x1,y1) )
+    # Y = m(x - x1) + y1
     plt.plot(a, (2*i)*a - (i**2))
 
 plt.show()

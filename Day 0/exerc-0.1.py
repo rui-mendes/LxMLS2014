d3abef7093b97d126fb34ee6e71dea1d459139cc
Rui Mendes <ruidamendes@ua.pt>
2014-07-22 14:51:18 +0100

    Changed Day 0 folder name (part2)

--- Day 0/exerc-0.1.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'ruimendes'
-
-countries = ['Portugal', 'Spain', 'United Kingdom']
-
-# Use L[i:j] to return the countries in the Iberian Peninsula.
-print countries[0:2]
--- Day 0/exerc-0.2.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'ruimendes'
-
-a=1
-while a <= 3:
-    print a
-a += 1
-
-# Indentation Error, infinite loop

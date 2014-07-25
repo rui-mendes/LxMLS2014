81c2bbd3d8aeb65600a2e09c7b7a6fbb9595f21f
Rui Mendes <ruidamendes@ua.pt>
2014-07-25 14:42:23 +0100

    Added confuion matrix calculation to exercise 3.2

--- Day3/exerc-3.2.py
+++ Day3/exerc-3.2.py
@@ -30,5 +30,19 @@ pred_test = crf_online.viterbi_decode_corpus(test_seq)
 eval_train = crf_online.evaluate_corpus(train_seq, pred_train)
 eval_dev = crf_online.evaluate_corpus(dev_seq, pred_dev)
 eval_test = crf_online.evaluate_corpus(test_seq, pred_test)
-print "CRF - Extended Features Accuracy Train: %.3f Dev: %.3f Test: %.3f"%(eval_train,
-                                                                           eval_dev,eval_test)
+print "CRF - Extended Features Accuracy Train: %.3f Dev: %.3f Test: %.3f"%(eval_train, eval_dev,eval_test)
+
+'''
+Compare the errors obtained with the two different feature sets. Do some error analysis: what errors were correct by
+using more features? Can you think of other features to use to solve the errors found?
+NOTE: to do some error analysis we can use confusion matrix to catch the major errors
+'''
+
+# Confusion_matrix calculation
+import lxmls.sequences.confusion_matrix as cm
+import matplotlib.pyplot as plt
+confusion_matrix = cm.build_confusion_matrix(test_seq.seq_list, pred_test, len(corpus.tag_dict),
+                                             crf_online.get_num_states())
+
+cm.plot_confusion_bar_graph(confusion_matrix, corpus.tag_dict,
+                            xrange(crf_online.get_num_states()), 'Confusion matrix')

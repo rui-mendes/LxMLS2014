54c0fd960ad9cbec97ff4e0f30aeb0de0a814bfc
Rui Mendes <ruidamendes@ua.pt>
2014-07-26 14:56:58 +0100

    Created exercise 4.2

---
+++ Day4/exerc-4.2.py
@@ -0,0 +1,8 @@
+__author__ = 'ruimendes'
+
+'''
+This exercise will show you that real-world sentences can have complicated syntactic structures. There is a
+parse tree visualizer in http://www.ark.cs.cmu.edu/treeviz/. Go to your local data/treebanks folder
+and open the file PTB excerpt.txt. Copy a few trees from the file, one at a time, and examine their parse trees in the
+visualizer.
+'''
